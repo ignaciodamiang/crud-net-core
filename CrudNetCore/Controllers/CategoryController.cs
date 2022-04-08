f30bbc6843f7d2766f5ec6b5cf8b3fafea47f52b
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-08 09:16:28 -0300

    Add Controller and View.

---
+++ CrudNetCore/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudNetCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
