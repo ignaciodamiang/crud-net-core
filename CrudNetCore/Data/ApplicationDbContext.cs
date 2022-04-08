c7acd1a6361ba6c135c47925eef4f058bd322164
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-08 09:14:38 -0300

    Set database.

---
+++ CrudNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNetCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
