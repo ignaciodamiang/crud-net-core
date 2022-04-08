ad5fdf499330e40945acf721bfc41f167117d47f
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-08 13:56:06 -0300

    access database from controller

--- CrudNetCore/Controllers/CategoryController.cs
+++ CrudNetCore/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudNetCore.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrudNetCore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
