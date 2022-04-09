9a26bfdc8b7d92a19959979d4a66f34278ac1122
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-09 17:14:36 -0300

    Custom Validation

--- CrudNetCore/Controllers/CategoryController.cs
+++ CrudNetCore/Controllers/CategoryController.cs
@@ -30,6 +30,10 @@ namespace CrudNetCore.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly math the Name.");
+            }
             if (ModelState.IsValid) 
             { 
             _db.Categories.Add(obj);
