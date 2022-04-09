d7768334db640ffe53fe2506bd003aa945d62736
ignaciodamiang <ignaciodamiang@gmail.com>
2022-04-09 17:58:13 -0300

    Edit Category - POST

--- CrudNetCore/Controllers/CategoryController.cs
+++ CrudNetCore/Controllers/CategoryController.cs
@@ -72,7 +72,7 @@ namespace CrudNetCore.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
